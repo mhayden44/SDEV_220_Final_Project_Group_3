52a02d42ea90ca486e1f5aaf1c28a4e833ee3166
mhayden44 <mhayden44@ivytech.edu>
2022-11-16 12:36:05 -0600

    commit

--- Profile/shoe/models.py
+++ Profile/shoe/models.py
@@ -8,13 +8,19 @@ fs = FileSystemStorage(location='/media/photos')
 # Create your models here.
 
 class Shoe(models.Model):
+    SHOE_TYPES=(
+        ('ATHL', 'Athletic'),
+        ('DRSS','Dress'),
+        ('WORK','Work'),
+        ('CASL','Casual'),
+    )
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     brand = models.CharField(max_length=100,null=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     size = models.CharField(max_length=200)
-    type = models.CharField(max_length=4)
+    type = models.CharField(max_length=4, choices=SHOE_TYPES,null=True)
     gender = models.CharField(max_length=1,null=True)
     photo = models.ImageField(storage=fs,null=True)
 
