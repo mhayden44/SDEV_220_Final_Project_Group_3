6ea82bc6c7944e1d002d657eb6fed8bdb4308e8c
mhayden44 <mhayden44@ivytech.edu>
2022-11-16 11:46:41 -0600

    commit

--- Profile/shoe/models.py
+++ Profile/shoe/models.py
@@ -1,18 +1,22 @@
 from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 from django.utils import timezone
+
+fs = FileSystemStorage(location='/media/photos')
 
 # Create your models here.
 
 class Shoe(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=20)
+    brand = models.CharField(max_length=100,null=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     size = models.CharField(max_length=200)
     type = models.CharField(max_length=4)
     gender = models.CharField(max_length=1,null=True)
-    photo = models.ImageField(null=True)
+    photo = models.ImageField(storage=fs,null=True)
 
     def __str__(self):
         return self.name
